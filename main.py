501cda199efe131589bf14d8b22c7d14ff34e3bc
rednt <47644820+rednt@users.noreply.github.com>
2025-01-29 14:30:03 +0100

    Implement email search functionality

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+
+# Project roadmap
+# Scope : - deleting emails older than {x} years
+# Protocol : IMAP
+# Goals : Delete emails based on conditions (e.g. older than {x} years , flagged as spam, etc)
+
+
+
 import imaplib
 import email
 import nltk
 import webbrowser
 import os
+import logging
 
 from dotenv import load_dotenv
 from email.header import decode_header
@@ -11,13 +20,63 @@ from nltk.corpus import stopwords
 
 # account credentials
 
-load_dotenv('./env/.env')
+load_dotenv('./env/.env') # Load the environment variables from the .env file [EMAIL, PASSWORD]
 
 imap_server = "imap.gmail.com"
 username = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 
 
+def main():
+    mail = connect_to_email()
+    print("Login successful.")
+    if mail:
+        mail.select("inbox")
+        search_condition(mail)
+        
+        mail.logout()
+        print("Logout successful.")
+
+# Search for emails based on a condition
+
+def search_condition(mail):
+    condition = input("Give me a condition to search for (options: UNSEEN, BEFORE DD-MMM-YYYY, SINCE DD-MMM-YYYY): ")
+    print(f"Searching for emails with condition: {condition}")
+    if condition.upper() == "UNSEEN":
+        status, email_ids = mail.search(None, "UNSEEN")
+    elif condition.upper().startswith("BEFORE"):
+        try:
+            date = condition.split(" ")[1]
+            status, email_ids = mail.search(None, f'BEFORE "{date}"')
+            print(f"Number of emails found: {len(email_ids[0].split())}")
+        except IndexError:
+            print("Invalid date format. Please use 'BEFORE DD-MMM-YYYY'.")
+            return
+    elif condition.upper().startswith("SINCE"):
+        try:
+            date = condition.split(" ")[1]
+            status, email_ids = mail.search(None, f'SINCE "{date}"')
+            print(f"Number of emails found: {len(email_ids[0].split())}")
+        except IndexError:
+            print("Invalid date format. Please use 'SINCE DD-MMM-YYYY'.")
+            return
+    else:
+        print("Invalid condition. Please use 'UNSEEN' or 'BEFORE DD-MMM-YYYY' or 'SINCE DD-MMM-YYYY'.")
+        return
+
+    email_list = email_ids[0].split()
+    print(f"Found {len(email_list)} emails matching the condition '{condition}'.")
+
+
+def fetch_email(mail, email_list):
+    for msg_id in email_list:
+        status, msg_data = mail.fetch(msg_id, "(RFC822)")
+        raw_email = msg_data[0][1]
+        msg = email.message_from_bytes(raw_email)
+        
+        
+
+# Connect to the email server
 def connect_to_email():
     try:
         # Connect to the server
@@ -31,52 +90,11 @@ def connect_to_email():
         return None
 
 
+
+
 if __name__ == "__main__":
-    mail = connect_to_email()
-    if mail:
-        
-        
-        mail.logout()
-        print("Logout successful!")
+    main()
 
 
-
-"""for email_id in email_ids[-1:]:
-    status, msg_data = mail.fetch(email_id, "(RFC822)")
-
-    for response_part in msg_data:
-        if isinstance(response_part, tuple):
-            # Parse the message into an email object
-            msg = email.message_from_bytes(response_part[1])
-
-            # Decode the email subject
-            subject, encoding = decode_header(msg["Subject"])[0]
-            if isinstance(subject, bytes):
-                # If it's a bytes type, decode to str
-                subject = subject.decode(encoding if encoding else "utf-8")
-            print("Subject:", subject)
-
-            # Decode the sender's email address
-            from_ = msg.get("From")
-            print("From:", from_)
-
-            # If the email message is multipart
-            if msg.is_multipart():
-                for part in msg.walk():
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-
-                    if "attachment" not in content_disposition:
-                        # Get the email body
-                        if content_type == "text/plain":
-                            body = part.get_payload(decode=True)
-                            print("Body:", body.decode())
-            else:
-                # The email body is not multipart
-                body = msg.get_payload(decode=True)
-                print("Body:", body.decode())
-
-# Logout from the server
-mail.logout()"""
 
 
